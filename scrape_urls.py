9aa79979ed075d1c00683c6d2a1559c3a7460644
Robby-78 <robinsonkoech78@gmail.com>
2023-07-13 18:09:56 +0300

    updated urls

--- scrape_urls.py
+++ scrape_urls.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://gileadtherapy.com/blog"
+url = "https://iconect.co.ke/"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
